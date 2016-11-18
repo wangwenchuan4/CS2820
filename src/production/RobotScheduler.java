af8edc1cd1972beeaf611aff5cfcf8f5ed0fb79a
marburgand <andrew-marburg@uiowa.edu>
2016-11-18 17:21:22 -0600

    made robot scheduler compilable

--- src/production/RobotScheduler.java
+++ src/production/RobotScheduler.java
@@ -3,5 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RobotScheduler implements Tickable{
+	public ArrayList<Robot> RobotList;
+	public ArrayList<Robot> AvailableRobots;
 	
-}
+	public RobotScheduler(){
+		
+	}
+		
+		public void tick(int i){
+			
+		}
+		
+	}
+	
+	
