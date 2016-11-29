fa47edc7c47cb674de6ff1d1d31d656f965461e9
wenchuan wang <wenchuan-wang@uiowa.edu>
2016-11-29 15:33:52 -0600

    update test

--- src/production/TestMaster.java
+++ src/production/TestMaster.java
@@ -26,4 +26,159 @@ public class TestMaster {
        
        
    }
+   
+   public class Floor implements Tickable,Task{
+		
+		Master master;
+		int currentTime;
+		
+		public Floor(Master m){
+			master = m;
+		}
+		
+		public void tick(int count){
+			currentTime = count;
+			
+		}
+		
+		public void fire(Object arg){
+			System.out.print(arg);
+			System.out.println(currentTime);
+			enqueue("Floor event happened at: ");
+		}
+		
+		public void enqueue(Object arg){
+			Event e = new Event(currentTime+4,arg,this);
+			master.enqueue(e);
+		}
+		
+		
+	}
+public class Inventory implements Tickable,Task{
+	
+	Master master;
+	int currentTime;
+
+	public Inventory(Master m){
+		master = m;
+	}
+	
+	public void tick(int count){
+		currentTime = count;
+		
+	}
+	
+	public void fire(Object arg){
+		System.out.print(arg);
+		System.out.println(currentTime);
+		enqueue("Inventory event happened at: ");
+	}
+	
+	public void enqueue(Object arg){
+		Event e = new Event(currentTime+6,arg,this);
+		master.enqueue(e);
+		
+	}
+	
+	
+}
+public class Order implements Tickable,Task{
+	
+	Master master;
+	int currentTime;
+
+	public Order(Master m){
+		master = m;
+	}
+	
+	public void tick(int count){
+		currentTime = count;
+		
+	}
+	
+	public void fire(Object arg){
+		System.out.print(arg);
+		System.out.println(currentTime);
+		
+		enqueue("Orders event happened at: ");
+	}
+	
+	public void enqueue(Object arg){
+		Event e = new Event(currentTime+5,arg,this);
+		master.enqueue(e);
+	}
+	
+	
+}
+/**
+*
+* @author wenchwang
+*/
+//assume robot event takes 5 tick
+public class Robot implements Tickable,Task {
+  	Master master;
+      int currentTime;
+		
+  public Robot(Master m){
+			master = m;
+		}
+  
+  
+  public void tick(int count){
+			currentTime = count;
+			
+		}
+		
+		
+  public void fire(Object arg){
+			System.out.print(arg);
+			System.out.println(currentTime);
+                       
+			enqueue("Robot event happened at: ");
+		}
+              
+  
+  public void enqueue(Object arg){
+			Event e = new Event(currentTime+5,arg,this);
+			// add another (future) event to the Master's queue
+                      master.enqueue(e);
+		}
+		
+
+        
+              
+}
+/**
+*
+* @author wenchwang
+*/
+//assume Visualizer event takes 3 tick
+public class Visualizer implements Tickable,Task{
+		
+		Master master;
+		int currentTime;
+
+		public Visualizer(Master m){
+			master = m;
+		}
+		
+		public void tick(int count){
+			currentTime = count;
+			
+		}
+		
+		public void fire(Object arg){
+			System.out.print(arg);
+			System.out.println(currentTime);
+			enqueue("Visualizer event happened at: ");
+		}
+		
+		public void enqueue(Object arg){
+			Event e = new Event(currentTime+3,arg,this);
+			master.enqueue(e);
+		}
+		
+		
+	}
+   
 }
